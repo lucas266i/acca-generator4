b7776dea38c378a087ac8cd9e41805ed6df50a6a
lucas266i <Empresario2115@gmail.com>
2026-09-12 12:46:23 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useCallback, useState } from 'react'
 import Auth from './components/Auth'
-import import { supabase } from './lib/supabase'
+import { supabase } from './lib/supabase'
 
 export default function App() {
   const [session, setSession] = useState(null)
